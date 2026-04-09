9361a5db5692a2410966ffe371c2ba20ea316c0a
Alex March <alexmarch@fastmail.com>
2026-04-09 19:37:30 +0900

    Add a void linux repo mirror service

---
+++ voidrepo/sync.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+rsync \
+  --recursive \
+  --links \
+  --hard-links \
+  --safe-links \
+  --perms \
+  --times \
+  --fuzzy \
+  --delete-delay \
+  --delay-updates \
+  --timeout=3600 \
+  --contimeout=120 \
+  --partial \
+  --partial-dir=.partial \
+  --no-motd \
+  --human-readable \
+  --verbose \
+  --progress \
+  --include="current/" \
+  --include="current/*.x86_64.*" \
+  --include="current/x86_64-repodata" \
+  --include="current/nonfree/" \
+  --include="current/nonfree/*.x86_64.*" \
+  --include="current/nonfree/x86_64-repodata" \
+  --include="current/bootstrap/" \
+  --include="current/bootstrap/*.x86_64.*" \
+  --include="current/bootstrap/x86_64-repodata" \
+  --include="man/" \
+  --include="man/x86_64/" \
+  --include="man/x86_64/**" \
+  --include="docs/" \
+  --include="docs/**" \
+  --include="xlocate/" \
+  --include="xlocate/**" \
+  --exclude="*" \
+  ${RSYNC_ARGS} \
+  "rsync://repo-sync.voidlinux.org/voidlinux" \
+  "/data/voidrepo"
